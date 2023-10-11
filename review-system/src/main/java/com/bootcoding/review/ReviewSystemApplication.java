34d0e4a2f5f875f9d5d1c32f6031e54e5b1e9611
prachihirekha <prachihirekhan32@gmail.com>
2023-10-11 13:58:12 +0530

    JDBC Template

---
+++ review-system/src/main/java/com/bootcoding/review/ReviewSystemApplication.java
@@ -0,0 +1,15 @@
+package com.bootcoding.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ReviewSystemApplication.class, args);
+		System.out.println("project run");
+	}
+
+}

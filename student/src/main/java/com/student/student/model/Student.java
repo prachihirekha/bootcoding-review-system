4f3a8c48f744307439529bff39e09db8038824a6
prachihirekha <prachihirekhan32@gmail.com>
2023-10-18 16:14:03 +0530

    JDBC Template

---
+++ student/src/main/java/com/student/student/model/Student.java
@@ -0,0 +1,13 @@
+package com.student.student.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private int id;
+    private String studentName;
+    private String gmailId;
+    private  long phoneNumber;
+    private  String address;
+
+}

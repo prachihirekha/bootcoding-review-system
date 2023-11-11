7926c19177ce5a8f11397a2c9c1b5c8bdd7b3d81
prachihirekha <prachihirekhan32@gmail.com>
2023-11-11 12:18:36 +0530

    JDBC Template

--- student/src/main/java/com/student/student/model/Course.java
+++ student/src/main/java/com/student/student/model/Course.java
@@ -1,9 +1,14 @@
 package com.student.student.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
     private  int id;
     private String name;
+
 }

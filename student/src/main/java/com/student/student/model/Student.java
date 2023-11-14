1e8e99f6cdb3b0fba569d92381b043181c57f330
prachihirekha <prachihirekhan32@gmail.com>
2023-11-14 19:29:25 +0530

    JDBC Template

--- student/src/main/java/com/student/student/model/Student.java
+++ student/src/main/java/com/student/student/model/Student.java
@@ -1,16 +1,27 @@
 package com.student.student.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private int id;
     private String studentName;
     private String gmailId;
     private  long phoneNumber;
     private  String address;
-    private List<Student> student;
+    private List<Course> courses;
 
+//    public List<Course> getCourses() {
+//        return courses;
+//    }
+//
+//    public void setCourses(List<Course> courses) {
+//        this.courses = courses;
+//    }
 }
